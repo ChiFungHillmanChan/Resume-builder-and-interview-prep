d4b02b166ea539797edcc1ac038672c380322ff4
ChiFungHillmanChan <107797011+ChiFungHillmanChan@users.noreply.github.com>
2024-10-24 02:34:30 +0100

    Update test_gemini.py

--- interview_prep/test_gemini.py
+++ interview_prep/test_gemini.py
@@ -2,12 +2,6 @@ import google.generativeai as genai
 import os
 import pprint
 
-genai.configure(api_key="AIzaSyCUhFvNw8JNmH5-17EbX2lK3XCssANVRJY")
-
-# response = genai.chat(messages=["Hello."])
-# print(response.last) #  'Hello! What can I help you with?'
-# response.reply("Can you tell me a joke?")
-
 model = genai.GenerativeModel('gemini-1.5-flash')
 response = model.generate_content("The opposite of hot is")
 print(response.text)
